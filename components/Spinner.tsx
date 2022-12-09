c5ced6cd48e930f72d5213e55946ae975aa5a1b4
Tariq Hamrit <contact@tariqhamrit.com>
2022-12-09 16:52:34 +0100

    fix: use proper svg properties

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -3,20 +3,8 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <svg
-      tw="animate-spin h-6 w-6 text-white"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-    >
-      <circle
-        tw="opacity-25"
-        cx="12"
-        cy="12"
-        r="10"
-        stroke="currentColor"
-        stroke-width="4"
-      ></circle>
+    <svg tw="animate-spin h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle tw="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
       <path
         tw="opacity-75"
         fill="currentColor"
